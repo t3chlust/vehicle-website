de8a16669cca08e8440d3ece53dfa0a31c91c79e
t3chlust <t3chlust@gmail.com>
2026-02-14 23:48:23 +0500

    a

--- migration.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Change verified column type from BIT(1) to TINYINT(1)
--- This allows storing values: 0 (pending), 1 (approved), 2 (rejected)
-
-ALTER TABLE `advertisement` MODIFY COLUMN `verified` TINYINT(1) NOT NULL DEFAULT 0;
